9bd8ed5a2f44e7a05aec98af6527262945337b71
nil88 <1351231599@qq.com>
2016-07-26 17:29:41 +0800

    First Commit

---
+++ src/ConfigIndex.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+
+use app\config\ConfigManager ;
+
+public calss ConfigIndex extends controller
+{
+     public function index()
+     {
+            return "config===about";
+     }
+
+}
+
+
+?>
---
+++ src/ConfigManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\config;
+
+public class ConfigManager{
+
+     public function showConfig()
+     {
+     	echo "show config";
+     }
+
+}
+
+
+?>
